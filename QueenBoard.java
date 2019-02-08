96fa1c20cc17059b650cc28bd17f35090110f379
Michael Chan <mchan10@stuy.edu>
2019-02-08 10:37:54 -0500

    added addQueen method

--- QueenBoard.java
+++ QueenBoard.java
@@ -3,4 +3,14 @@ public class QueenBoard{
   public QueenBoard(int size){
     board = new int[size][size];
   }
+  private boolean addQueen(int r, int c){
+    board[r][c] += 1;
+    for (int i = 1; r + i < board.length){
+      board[r + i][c] -= 1;
+    }
+    for (int i = 1; r + i < board.length && c + i < board.length){
+      board[r + i][c + i] -= 1;
+    }
+    return true;
+  }
 }
